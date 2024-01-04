3a87c683aa1fea51b2dd81fa69d1de2a2a85f633
Mafoa <steve_mafoa@outlook.com>
2024-01-04 12:30:19 +0100

    Finition de la barre latéral

--- app-web-divertissement/src/components/Aside.jsx
+++ app-web-divertissement/src/components/Aside.jsx
@@ -1,5 +1,40 @@
+import { Link, Outlet } from "react-router-dom"
+import { TiThSmall } from "react-icons/ti";
+import { MdOutlineLocalMovies } from "react-icons/md";
+import { MdMovie } from "react-icons/md";
+import { GoBookmark } from "react-icons/go";
+import { PiTelevision } from "react-icons/pi";
+
 export default function Aside() {
   return (
-    <div className="p-2 text-slate-600 bg-black">Aside</div>
+    <div className="bg-[#171d2f] flex justify-between flex-col h-[650px] rounded-[20px] p-5 flex-wrap">
+      <div className="h-10">
+        <p className="flex text-[#fc4544] justify-center text-[30px] h-1/3"><MdMovie /></p>
+      </div>
+     <div className="h-1/3">
+        <nav className="flex flex-col gap-10 items-center">
+            <Link 
+            to="/categories/all"
+            className="mx-2 font-semibold text-[25px] text-[#fbfafd]"
+            > <TiThSmall /></Link>
+            <Link 
+            to="/categories/movie"
+            className="mx-2 font-semibold text-[25px] text-[#fbfafd] "
+            ><MdOutlineLocalMovies /></Link>
+            <Link 
+            to="/categories/serie"
+            className="mx-2 font-semibold text-[25px] text-[#fbfafd]"
+            ><PiTelevision /></Link>
+            <Link 
+            to="/categories/favoris"
+            className="mx-2 font-semibold text-[25px] text-[#fbfafd]"
+            ><GoBookmark /></Link>
+        </nav>
+        <Outlet />
+    </div>
+    <div className=" h-1/3 flex justify-end flex-col rounded-[50%] items-center pb-2 text-[20px]">
+      <p>😎</p>
+    </div>
+   </div>
   )
 }
