f0b16daeab53fab2aab00ec7a807ca8e8e89569f
Mafoa <steve_mafoa@outlook.com>
2024-01-04 13:47:36 +0100

    Ajout de la barre de recherche

--- app-web-divertissement/src/categories/All.jsx
+++ app-web-divertissement/src/categories/All.jsx
@@ -1,7 +1,13 @@
+import { IoSearchOutline } from "react-icons/io5";
+
 export default function All() {
   return (
-    <div>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate, minus non asperiores odit hic veritatis, pariatur accusamus possimus, repellendus distinctio ipsa quos voluptatem qui iusto?</p>
-    </div>
+    <section className="m-6 pb-10">
+        <div className="flex items-center gap-4 text-[18px] pb-10">
+          <p className="text-[25px] text-slate-50"><IoSearchOutline /></p>
+          <input type="text" placeholder="Search for movies or TV series" className="w-96 outline-none bg-transparent" />
+        </div>
+        <h1 className="text-3xl text-slate-200">Trending</h1>
+    </section>
   )
 }
