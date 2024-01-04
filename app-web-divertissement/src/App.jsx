9b20a07c4a8db335b3f376f89f7437f9af06513b
Mafoa <steve_mafoa@outlook.com>
2024-01-04 12:29:01 +0100

    Ajout des routes

--- app-web-divertissement/src/App.jsx
+++ app-web-divertissement/src/App.jsx
@@ -1,13 +1,33 @@
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Aside from './components/Aside'
+import './App.css'
+import All from './categories/All'
+import Movie from "./categories/Movie"
+import Serie from './categories/Serie'
+import Favoris from "./categories/Favoris"
 
 function App() {
   
 
   return (
-    <>
+    <div className="flex p-8">
+
+      <BrowserRouter>
       <Aside />
-    </>
+        <Routes>
+          <Route path="/"/>
+
+            <Route path="categories/All" element={<All />}/>
+            <Route path="categories/Movie" element={<Movie />}/>
+            <Route path="categories/Serie" element={<Serie />}/>
+            <Route path="categories/Favoris" element={<Favoris />}/>
+            
+
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
+
+    </div>
   )
 }
 
