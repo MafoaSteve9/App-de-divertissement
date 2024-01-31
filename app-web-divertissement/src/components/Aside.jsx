ba799aad48d5f02a20de9001b1938dc6b0a9d33b
Mafoa <steve_mafoa@outlook.com>
2024-01-31 18:09:12 +0100

    Changement de la couleurs des icons  par rapport à l'endroit ou on se stiue

--- app-web-divertissement/src/components/Aside.jsx
+++ app-web-divertissement/src/components/Aside.jsx
@@ -1,40 +1,53 @@
 import { Link, Outlet } from "react-router-dom"
-import { TiThSmall } from "react-icons/ti";
-import { MdOutlineLocalMovies } from "react-icons/md";
+import { useLocation } from "react-router-dom";
+import { TbMovie } from "react-icons/tb";
 import { MdMovie } from "react-icons/md";
-import { GoBookmark } from "react-icons/go";
+import { FaBookmark } from "react-icons/fa6";
+import { HiMiniSquares2X2 } from "react-icons/hi2";
 import { PiTelevision } from "react-icons/pi";
+import '../App.css'
 
 export default function Aside() {
+const location = useLocation()
+
   return (
-    <div className="bg-[#171d2f] flex justify-between flex-col h-[650px] rounded-[20px] p-5 flex-wrap">
-      <div className="h-10">
-        <p className="flex text-[#fc4544] justify-center text-[30px] h-1/3"><MdMovie /></p>
-      </div>
-     <div className="h-1/3">
-        <nav className="flex flex-col gap-10 items-center">
-            <Link 
-            to="/categories/all"
-            className="mx-2 font-semibold text-[25px] text-[#fbfafd]"
-            > <TiThSmall /></Link>
-            <Link 
-            to="/categories/movie"
-            className="mx-2 font-semibold text-[25px] text-[#fbfafd] "
-            ><MdOutlineLocalMovies /></Link>
-            <Link 
-            to="/categories/serie"
-            className="mx-2 font-semibold text-[25px] text-[#fbfafd]"
-            ><PiTelevision /></Link>
-            <Link 
-            to="/categories/favoris"
-            className="mx-2 font-semibold text-[25px] text-[#fbfafd]"
-            ><GoBookmark /></Link>
-        </nav>
-        <Outlet />
+    <div className="bg-[#171d2f] flex lg:flex-row sm:flex-col lg:h-[650px] lg:rounded-2xl md:rounded-2xl p-5 flex-wrap">
+  <nav className="flex-col-nav flex lg:flex-col items-center justify-between">
+    <div>
+      <p className="flex text-[#fc4544] justify-center text-[30px] h-1/3"><MdMovie /></p>
     </div>
-    <div className=" h-1/3 flex justify-end flex-col rounded-[50%] items-center pb-2 text-[20px]">
-      <p>😎</p>
+    <div className="flex lg:flex-col sm:flex-row sm:gap-5 lg:gap-5 xl:gap-5">
+    <Link
+        to="/"
+        className={`mx-2 text-[25px] text-white ${location.pathname === '/' && ' text-[#d1c9c9]'}`}
+      >
+        <HiMiniSquares2X2 />
+      </Link>
+      <Link
+        to="/categories/Movie"
+        className={`mx-2 text-[25px] text-white ${location.pathname === '/categories/Movie' && 'font-semibold text-[#d1c9c9]'}`}
+      >
+        <TbMovie />
+      </Link>
+      <Link
+        to="/categories/Serie"
+        className={`mx-2 text-[25px] text-white ${location.pathname === '/categories/Serie' && 'font-semibold text-[#d1c9c9]'}`}
+      >
+        <PiTelevision />
+      </Link>
+      <Link
+        to="/categories/Favoris"
+        className={`mx-2 text-[25px] text-white ${location.pathname === '/categories/Favoris' && 'font-semibold text-[#d1c9c9]'}`}
+      >
+        <FaBookmark />
+      </Link>
     </div>
-   </div>
+    <div>
+      <p className="text-[25px]">😎</p>
+    </div>
+  </nav>
+  <Outlet />
+</div>
+
   )
 }
